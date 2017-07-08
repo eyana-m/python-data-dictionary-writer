b9b6d453cf9c98f1932b3de46d520d4c9fe3951f
Eyana <emallari@exist.com>
2017-07-08 08:21:23 +0800

    added sample data, experimented reading html

--- readhtml.py
+++ readhtml.py
@@ -1,1 +1,20 @@
 ##First Commit
+
+from bs4 import BeautifulSoup
+
+# create new link
+#new_link = soup.new_tag("link", rel="icon", type="image/png", href="img/tor.png")
+# insert it into the document
+#soup.head.append(new_link)
+
+#new_tag = soup.new_tag('div', id='description')
+#soup.body.append(new_tag)
+#print(soup.title)
+
+with open("tables/cfg_billing_id.html") as inf:
+    txt = inf.read()
+    soup = BeautifulSoup(txt, 'html.parser')
+
+###print(soup.find_all('table'))
+
+print(soup.name)
