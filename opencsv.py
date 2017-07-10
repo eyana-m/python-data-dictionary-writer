7901e5d56f727140ee7347dba89a39da6f547eb8
Eyana Mallari <eyanamallari@eyana.local>
2017-07-10 21:38:17 +0800

    retrieve csv file using python

--- opencsv.py
+++ opencsv.py
@@ -4,7 +4,12 @@ import os
 
 base=os.path.basename("../../Google Drive/Python/CSV_dump/Settlement-Tables-Descriptions.csv")
 
-with open("../../Google Drive/Python/CSV_dump/Settlement-Tables-Descriptions.csv", "rb") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print row
+def openCSV(table):
+    with open("../../Google Drive/Python/CSV_dump/Settlement-Tables-Descriptions.csv", "rb") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == table:
+                return row[1]
+                break;
+
+print openCSV("cfg_billing_id")
--- writetabledescription.py
+++ writetabledescription.py
@@ -1,18 +1,19 @@
 import os
+import csv
 from bs4 import BeautifulSoup
 
-# create new link
-#new_link = soup.new_tag("link", rel="icon", type="image/png", href="img/tor.png")
-# insert it into the document
-#soup.head.append(new_link)
-
-#new_tag = soup.new_tag('div', id='description')
-#soup.body.append(new_tag)
-#print(soup.title)
+def openCSV(table):
+    with open("../../Google Drive/Python/CSV_dump/Settlement-Tables-Descriptions.csv", "rb") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == table:
+                return row[1]
+            break;
+print "--------------------------------------------"
+print "Writing to SchemaSpy Data Dictionary..."
+print "--------------------------------------------"
 
 base=os.path.basename("Data/settlement/tables/cfg_billing_id.html")
-
-print(base)
 
 with open("Data/settlement/tables/"+base) as inf:
     txt = inf.read()
